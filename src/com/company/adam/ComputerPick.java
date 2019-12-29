1b9f30f6d4e4895186ae7f5c93e3ff21f55d99f0
Adam <adam.vichik@gmail.com>
2019-12-29 22:42:52 +0200

    Create RandomPick() method and FreeIndices() method

--- src/com/company/adam/ComputerPick.java
+++ src/com/company/adam/ComputerPick.java
@@ -1,8 +1,26 @@
 package com.company.adam;
+import java.util.*;
+
+
 
 public class ComputerPick {
+    Random rnd = new Random();
+    ArrayList<Integer> positionArray = new ArrayList<Integer>(9);
 
-    public int randomPick(){
+    public int[][] randomPick(int[][] state, int[][] arr){
+        ArrayList<Integer> tmp = FreeIndices(state, arr);
 
+        // use length of tmp to select next move
+        int position = rnd.nextInt(10) + 1;
+
+        // Update state with the selection
+        state[i][j] = 1;
+        return state;
     }
+
+    public ArrayList<Integer> FreeIndices(int[][] state,int[][] arr){
+        // return all indices of free cells in state
+    }
+
+
 }
--- src/com/company/adam/Main.java
+++ src/com/company/adam/Main.java
@@ -5,5 +5,7 @@ public class Main {
     ComputerPick computerpick = new ComputerPick();
     public static void main(String[] args) {
 	// write your code here
+        int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] state = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
     }
 }
