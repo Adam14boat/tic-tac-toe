53e6bdd3d344781545188d71d9e88a2c36c2e293
Adam <adam.vichik@gmail.com>
2019-12-29 21:58:38 +0200

    Create computerpick and userinput instances

--- src/com/company/adam/Main.java
+++ src/com/company/adam/Main.java
@@ -1,7 +1,8 @@
 package com.company.adam;
 
 public class Main {
-
+    UserInput userinput = new UserInput();
+    ComputerPick computerpick = new ComputerPick();
     public static void main(String[] args) {
 	// write your code here
     }
