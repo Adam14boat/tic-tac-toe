d3fc93dd2e3ba60fcd65bc05bed6981228e00830
Adam <adam.vichik@gmail.com>
2019-12-30 08:53:36 +0200

    Create hasSomeoneWon() method

--- src/com/company/adam/Main.java
+++ src/com/company/adam/Main.java
@@ -1,11 +1,24 @@
 package com.company.adam;
 
 public class Main {
-    UserInput userinput = new UserInput();
-    ComputerPick computerpick = new ComputerPick();
+    static UserInput userinput = new UserInput();
+    static ComputerPick computerpick = new ComputerPick();
     public static void main(String[] args) {
 	// write your code here
         int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         int[][] state = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+        for (int i = 1; i <= 4; i++) {
+            userinput.getInput(state, arr);
+            System.out.println(state);
+            computerpick.randomPick(state, arr);
+            System.out.println(state);
+        }
+        userinput.getInput(state, arr);
+        System.out.println(state);
+    }
+
+    public boolean hasSomeoneWon() {
+
+        return
     }
 }
