21070819e193451958c5d187db42c70835034850
Adam <adam.vichik@gmail.com>
2019-12-30 16:19:18 +0200

    Delete unnecessary code

--- src/com/company/adam/UserInput.java
+++ src/com/company/adam/UserInput.java
@@ -10,15 +10,6 @@ public class UserInput {
         System.out.println("Enter the number of the cell you want to place your piece in");
         cell = input.nextInt();
         boolean cell_not_placed = true;
-        for (int i = 0; i <= 2; i++) {
-            for (int j = 0; j <= 2; j++) {
-                System.out.println(arr[i][j]);
-                if (arr[i][j] == cell && state[i][j] == 0) {
-                    cell_not_placed = false;
-                    state[i][j] = 1;
-                }
-            }
-        }
         while (cell_not_placed) {
             System.out.println("Enter the number of the cell you want to place your piece in");
             cell = input.nextInt();
