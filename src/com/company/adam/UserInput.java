91e7f9fc7cc51fd164bf2cbc5830b3a677c27ea4
Adam <adam.vichik@gmail.com>
2019-12-30 08:51:46 +0200

    Utilize getInput()  method

--- src/com/company/adam/UserInput.java
+++ src/com/company/adam/UserInput.java
@@ -3,12 +3,34 @@ package com.company.adam;
 import java.util.*;
 
 public class UserInput {
-    public int position;
+    private int cell;
     Scanner input = new Scanner(System.in);
 
-    public int getInput(){
-        System.out.println("Enter the number of the square you want to place your piece in");
-        position = input.nextInt();
-        return position;
+    public int[][] getInput(int[][] state, int[][] arr){
+        System.out.println("Enter the number of the cell you want to place your piece in");
+        cell = input.nextInt();
+        boolean cell_not_placed = true;
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                if (arr[i][j] == cell && state[i][j] == 0) {
+                    cell_not_placed = false;
+                    state[i][j] = 1;
+                }
+            }
+        }
+        while (cell_not_placed) {
+            System.out.println("Enter the number of the cell you want to place your piece in");
+            cell = input.nextInt();
+            for (int i = 1; i <= 3; i++) {
+                for (int j = 1; j <= 3; j++) {
+                    if (arr[i][j] == cell && state[i][j] == 0) {
+                        cell_not_placed = false;
+                        state[i][j] = 1;
+                    }
+                }
+            }
+        }
+        return state;
     }
+
 }
