a34c76fb6c13e713126a0a0626fb2fae3b4f89e1
Adam <adam.vichik@gmail.com>
2019-12-30 16:19:48 +0200

    Utilize hasUserWon() method

--- src/com/company/adam/Main.java
+++ src/com/company/adam/Main.java
@@ -3,8 +3,8 @@ package com.company.adam;
 import java.util.Arrays;
 
 public class Main {
-    static UserInput userinput = new UserInput();
-    static ComputerPick computerpick = new ComputerPick();
+    public static UserInput userinput = new UserInput();
+    public static ComputerPick computerpick = new ComputerPick();
     public static void main(String[] args) {
 	// write your code here
         int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
@@ -12,20 +12,20 @@ public class Main {
         System.out.println(Arrays.deepToString(state));
         for (int i = 1; i <= 4; i++) {
             userinput.getInput(state, arr);
+            if (userinput.hasUserWon(state)) {
+                System.out.println("Congratulations you won!!");
+                return;
+            }
             System.out.println(Arrays.deepToString(state));
             computerpick.randomPick(state, arr);
             System.out.println(Arrays.deepToString(state));
         }
         userinput.getInput(state, arr);
+        if (userinput.hasUserWon(state)) {
+            System.out.println("Congratulations you won!!");
+            return;
+        }
         System.out.println(Arrays.deepToString(state));
     }
 
-    /**
-     * check if someone won
-     * @return
-     */
-    public boolean hasSomeoneWon() {
-
-        return false;
-    }
 }
