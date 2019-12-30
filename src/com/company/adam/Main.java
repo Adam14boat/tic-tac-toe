3b6824935e3c5624fa005f6c772f1cb91fb3ce93
Adam <adam.vichik@gmail.com>
2019-12-30 13:23:50 +0200

    Print state using deepToString() method

--- src/com/company/adam/Main.java
+++ src/com/company/adam/Main.java
@@ -1,5 +1,8 @@
 package com.company.adam;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     static UserInput userinput = new UserInput();
     static ComputerPick computerpick = new ComputerPick();
@@ -7,18 +10,23 @@ public class Main {
 	// write your code here
         int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         int[][] state = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+        System.out.println(Arrays.deepToString(state));
         for (int i = 1; i <= 4; i++) {
             userinput.getInput(state, arr);
-            System.out.println(state);
+            System.out.println(Arrays.deepToString(state));
             computerpick.randomPick(state, arr);
-            System.out.println(state);
+            System.out.println(Arrays.deepToString(state));
         }
         userinput.getInput(state, arr);
-        System.out.println(state);
+        System.out.println(Arrays.deepToString(state));
     }
 
+    /**
+     * check if someone won
+     * @return
+     */
     public boolean hasSomeoneWon() {
 
-        return
+        return false;
     }
 }
