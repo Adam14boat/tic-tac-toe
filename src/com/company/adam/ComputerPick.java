89467f2675fb1b780a789686592b5e76109115e1
Adam <adam.vichik@gmail.com>
2019-12-30 07:53:40 +0200

    Utilize randomPick() and FreeIndices() methods

--- src/com/company/adam/ComputerPick.java
+++ src/com/company/adam/ComputerPick.java
@@ -5,21 +5,44 @@ import java.util.*;
 
 public class ComputerPick {
     Random rnd = new Random();
-    ArrayList<Integer> positionArray = new ArrayList<Integer>(9);
 
+    /**
+     * receives the state and arr arrays and randomly chooses and places an index that is free
+     * @param state the state of the game currently
+     * @param arr an array in which each number corresponds to a cell
+     * @return 2 in the free cell of state
+     */
     public int[][] randomPick(int[][] state, int[][] arr){
         ArrayList<Integer> tmp = FreeIndices(state, arr);
-
         // use length of tmp to select next move
-        int position = rnd.nextInt(10) + 1;
-
+        int index = rnd.nextInt(tmp.size());
         // Update state with the selection
-        state[i][j] = 1;
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                if (arr[i][j] == index) {
+                    state[i][j] = 2;
+                }
+            }
+        }
         return state;
     }
 
-    public ArrayList<Integer> FreeIndices(int[][] state,int[][] arr){
+    /**
+     * @param state
+     * @param arr
+     * @return the free cells that are represented in array tmp
+     */
+    public ArrayList<Integer> FreeIndices(int[][] state, int[][] arr){
         // return all indices of free cells in state
+        ArrayList<Integer> tmp = new ArrayList<Integer>(0);
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                if (state[i][j] == 0) {
+                    tmp.add(arr[i][j]);
+                }
+            }
+        }
+        return tmp;
     }
 
 
